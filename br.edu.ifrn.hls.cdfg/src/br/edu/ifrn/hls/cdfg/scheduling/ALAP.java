e0bdea96382214d3becc9530d69a380374c456c0
Mateus Oliveira <mateusocb@gmail.com>
2015-07-22 22:16:45 -0300

    adding alap v2

--- br.edu.ifrn.hls.cdfg/src/br/edu/ifrn/hls/cdfg/scheduling/ALAP.java
+++ br.edu.ifrn.hls.cdfg/src/br/edu/ifrn/hls/cdfg/scheduling/ALAP.java
@@ -17,14 +17,20 @@ import br.edu.ifrn.hls.cdfg.visualization.DFGDOTFormatGenerator;
 
 public class ALAP {
 
+  private List<String> ready = new ArrayList<String>();
+  private int largerAsap = 0;
+  private int total;
+
 	public void schedule(DFG dfg) {
 		ASAP asap = new ASAP();
 		asap.schedule(dfg);
+		total = dfg.getInputs().size() + dfg.getOutputs().size() 
+		  + dfg.getOperations().size();
+		System.out.println(total);
 		removeALAP(dfg);
-		equalizeIO(dfg);
-		scheduleOutputs(dfg);
-		scheduleOperations(dfg);
-		scheduleFinal(dfg);
+    equalizeOutputs(dfg);
+	  scheduleInputs(dfg);
+	  scheduleOperations(dfg);
 	}
 
 	private void removeALAP(DFG dfg) {
@@ -38,61 +44,46 @@ public class ALAP {
 			out.getTags().remove("alap");
 		}
 	}
-	
-	private void scheduleOperations(DFG dfg) {
-		boolean temp = true;
-		while(temp){
-			temp = false;
-			for(DFGOperationNode op : dfg.getOperations().values()){
-				for(DFGNodePort node : op.getOutputs().values()){
-					int maior = 0;
-					for(DFGNode target : node.getConnectedTo().getTargetsNodes())
-						if(target instanceof DFGOperationNode){
-							DFGOperationNode oper = (DFGOperationNode) target;
-							if(oper.getTags().get("alap") != null){
-								int valor = Integer.parseInt(oper.getTags().get("alap"));
-								if(valor > maior){
-									op.getTags().put("alap", String.valueOf(--valor));
-									maior = valor;
-								}
-							} else {
-								temp = false;
-							}
-					}
-				}
-			}
-		}
-	}
 
-	private void scheduleOutputs(DFG dfg){
-		for(DFGOutputNode out : dfg.getOutputs().values()){
-			for(DFGNodePort input : out.getInputs().values()){
-				int value =  Integer.parseInt(out.getTags().get("alap"));
-				input.getConnectedTo().getSource().getNode().getTags().put("alap", String.valueOf(value-1));
-			}
-		}
+	private void equalizeOutputs(DFG dfg) {
+	  for (DFGOutputNode out : dfg.getOutputs().values()) {
+      ready.add(out.getName());
+      if (Integer.valueOf(out.getTags().get("asap")) > largerAsap) {
+        largerAsap = Integer.valueOf(out.getTags().get("asap"));
+      }
+	  }
+	  for (DFGOutputNode out : dfg.getOutputs().values()) {
+	    out.getTags().put("alap", String.valueOf(largerAsap));
+	  }
 	}
-	
-	private void equalizeIO(DFG dfg){
-		int bigger = 0;
-		for(DFGOutputNode out : dfg.getOutputs().values()){
-			if(Integer.parseInt(out.getTags().get("asap")) > bigger){
-				bigger = Integer.parseInt(out.getTags().get("asap"));
-			}
-		}
-		for(DFGOutputNode out : dfg.getOutputs().values()){
-			out.getTags().put("alap", String.valueOf(bigger));
-		}
-	}
+  private void scheduleInputs(DFG dfg) {
+    for(DFGInputNode in : dfg.getInputs().values()) {
+      in.getTags().put("alap", "0");
+      ready.add(in.getName());
+    }
+  }
 
-	private void scheduleFinal(DFG dfg) {
-		for(DFGOperationNode op : dfg.getOperations().values()){
-			if(op.getTags().get("alap") == null){
-				op.getTags().put("alap", "1");
-			}
-		}
-		for(DFGInputNode in : dfg.getInputs().values()){
-				in.getTags().put("alap", "0");
-		}
-	}
+  private void scheduleOperations(DFG dfg) {
+    while(ready.size() < total) {
+      for (DFGOperationNode op : dfg.getOperations().values()) {
+        if(!ready.contains(op.getName())){
+          for(DFGNodePort port : op.getOutputs().values()) {
+            int smallAlap = 10;
+            for(DFGNode node : port.getConnectedTo().getTargetsNodes()){
+              if (node.getTags().get("alap") != null) {
+                if (Integer.valueOf(node.getTags().get("alap")) < smallAlap) {
+                  smallAlap = Integer.valueOf(node.getTags().get("alap"));
+                  op.getTags().put("alap", String.valueOf(smallAlap-1));
+                  if (!ready.contains(op.getName())) {
+                    ready.add(op.getName());
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
+
